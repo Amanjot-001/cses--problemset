8332b87505f101c38df751ba635b1b856c9687ec
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-08-05 22:24:58 +0530

    .

---
+++ graphs/Planets_Queries_1.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int32_t main() {
+	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	int n, q;
+	cin >> n >> q;
+	
+	return 0;
+}
