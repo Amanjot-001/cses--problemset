b54fa2e89f4dce82510867dadd49f0f08ef0dd7e
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-12-30 15:40:52 +0530

    solved https://cses.fi/problemset/result/11597713/

--- dynamic-programming/increasing-subsequence.cpp
+++ dynamic-programming/increasing-subsequence.cpp
@@ -1,25 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
+
+const int INF = 1e9;
 
 int main() {
-    int n;
-    cin >> n;
-    vector<int> arr(n);
-    for(int i=0; i<n; i++)
-        cin >> arr[i];
-    
-    vector<int> dp;
-    dp.push_back(arr[0]);
+    ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-    for(int i=1; i<n; i++) {
-        auto it = lower_bound(dp.begin(), dp.end(), arr[i]);
-        
-        if(it == dp.end())
-            dp.push_back(arr[i]);
-        else
-            *it = arr[i];
-    }
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for(int i=0; i<n; i++)
+		cin >> a[i];
 
-    cout << dp.size() << endl;
+	vector<int> dp(n+1, INF);
+	dp[0] = -INF;
+
+	for(int i=0; i<n; i++) {
+		int l = upper_bound(dp.begin(), dp.end(), a[i]) - dp.begin(); // element just bigger than a[i]
+
+		if(dp[l-1] < a[i] && a[i] < dp[l])
+			dp[l] = a[i];
+	}
+
+	int ans = 0;
+	for(int l=0; l<=n; l++) {
+		if(dp[l] < INF)
+			ans = l;
+	}
+
+	cout << ans << endl;
+	
     return 0;
 }
